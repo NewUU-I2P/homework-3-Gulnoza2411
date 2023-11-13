25feff3b19cb8a056fc529f0dca5f8b781e43448
SRK_admin <g.komilova@newuu.uz>
2023-11-13 21:00:34 +0500

    1.2.3.4.5

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,14 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
-
+    if (i>j && i>k){
+        cout<<"the maximum number is i="<<i<<endl;
+    }
+    else if (j>i && j>k){
+        cout<<"the maximum number is j="<<j<<endl;
+    }
+    else  (k>i && k>j){
+        cout<<"the maximum number is k="<<k<<endl;
+    }
     return result;
 }
--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,27 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
    // write your code here
-
+    switch () {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                cout << "Error" << endl;
+                return 1;
+            }
+            break;
+        default:
+            cout << "Error" << endl;
+            return 1;
+    }
    return result;
 }
