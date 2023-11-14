d837cea7ae6838a3e8a6acb369267cfde2d69f3f
SRK_admin <g.komilova@newuu.uz>
2023-11-14 19:53:24 +0500

    1.2.3.4.5

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,6 @@
 float problemSolution2(float i, float j, float k) {
     float result=(i> j)? (i> k)? i: k :(j>k)? j: k);
 //write your code here
-    }
-    return result;
+}
+return result;
 }
--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,6 +1,6 @@
 #include <string>
 
-std::string problemSolution3(float height, char S) {
+string problemSolution3(float height, char S) {
     std::string result;
     if(S=='M'){
         if(height<1.70){
@@ -12,18 +12,18 @@ std::string problemSolution3(float height, char S) {
         else{
             result="tall";
         }
-       else if(S=='F'){
-           if(height<1.6){
-               result="short";
-           }
-           else if (height>=1.6 && height<1.75){
-               result="normal";
-           }
-           else{
-               result="tall";
-           }
-       }
-       return result;
+        else if(S=='F'){
+            if(height<1.6){
+                result="short";
+            }
+            else if (height>=1.6 && height<1.75){
+                result="normal";
+            }
+            else{
+                result="tall";
+            }
+        }
+        return result;
     }
     // write your code here
 
