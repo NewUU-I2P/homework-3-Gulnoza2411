b20dfac0303c05188dcaea83ae30e4a9e403af06
SRK_admin <g.komilova@newuu.uz>
2023-11-14 20:01:27 +0500

    1.2.3.4.5

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,17 @@
-float problemSolution2(float i, float j, float k) {
-    float result=(i> j)? (i> k)? i: k :(j>k)? j: k);
-//write your code here
-}
-return result;
+#include <stdio.h>
+
+int main() {
+    float price1, price2, price3;
+
+    // Input
+    printf("Enter the prices of three products: ");
+    scanf("%f %f %f", &price1, &price2, &price3);
+
+    // Find the maximum using the ternary operator
+    float maxPrice = (price1 > price2) ? ((price1 > price3) ? price1 : price3) : ((price2 > price3) ? price2 : price3);
+
+    // Output
+    printf("The maximum number is %.2f\n", maxPrice);
+
+    return 0;
 }
