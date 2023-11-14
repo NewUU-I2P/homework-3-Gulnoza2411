5082aa18c6f470abaf410d2af2e3c49b1d9623f3
SRK_admin <g.komilova@newuu.uz>
2023-11-14 20:04:31 +0500

    1.2.3.4.5

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,26 +2,31 @@
 
 std::string problemSolution3(float height, char S) {
     std::string result;
-
-    if (S== 'M') {
-        if (height < 1.7) {
-            result = "Short";
-        } else if (height >= 1.7 && height < 1.85) {
-            result = "Normal";
-        } else {
-            result = "Tall";
+    if(s=='M'){
+        if(height<1.7){
+            result="short";
         }
-    } else if (sex == 'F') {
-        if (height < 1.6) {
-            result = "Short";
-        } else if (height >= 1.6 && height < 1.75) {
-            result = "Normal";
-        } else {
-            result = "Tall";
+        else if (height>=1.7 && height<1.85){
+            result="normal";
         }
-    } else {
-        result = "Invalid input for S. Please enter 'M' or 'F'.";
+        else{
+            result="tall";
+        }
+        else if(S=='F'){
+            if(height<1.6){
+                result="short";
+            }
+            else if (height>=1.6 && height<1.75){
+                result="normal";
+            }
+            else{
+                result="tall"
+            }
+        }
+        return result;
     }
+    // write your code here
 
-    return result;
+    // use return to return your result
+    // make use of control flow statements
 }
